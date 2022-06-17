360882df9b15d2b47387f486fbdb73c4c2bbf262
lublagg <lvblagg@gmail.com>
2022-06-17 11:31:30 -0400

    Shoreline meets elevation of the terrain.

--- src/components/helpers.tsx
+++ src/components/helpers.tsx
@@ -2,3 +2,4 @@ export const terrainWidth = 40;
 export const gridWidth = 3;
 export const gridLength = 6;
 export const terrainLength = 14;
+export const waterLength = 4;
--- src/components/terrain.tsx
+++ src/components/terrain.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { renderPlane } from "./planeHelper";
-import { terrainWidth, terrainLength, gridLength, gridWidth } from "./helpers";
+import { terrainWidth, terrainLength, gridLength, gridWidth, waterLength } from "./helpers";
 import { ITransectPoint } from "../data/fake-data";
 import sand from "../assets/sand.png";
+import water from "../assets/water.png";
 
 interface TerrainProps {
   data: Array<ITransectPoint>
@@ -17,13 +18,19 @@ export const Terrain = (props: TerrainProps) => {
   const leftSideRef = useRef<THREE.BufferGeometry>();
   const backSideRef = useRef<THREE.BufferGeometry>();
 
+  const waterRef = useRef<THREE.BufferGeometry>();
+  const waterRightSideRef = useRef<THREE.BufferGeometry>();
+  const waterLeftSideRef = useRef<THREE.BufferGeometry>();
+
   const sandTexture = new THREE.TextureLoader().load(sand);
+  const waterTexture = new THREE.TextureLoader().load(water);
 
   useEffect(() => {
     setTerrainElevation();
     setSideElevation("right");
     setSideElevation("left");
     setSideElevation("back");
+    setWaterElevation();
   });
 
 
@@ -51,9 +58,13 @@ export const Terrain = (props: TerrainProps) => {
     return transectData;
   };
 
+  const getPositionArray = (currentObject: THREE.BufferGeometry) => {
+    return currentObject.attributes.position.array as number[];
+  };
+
   const setSideElevation = (type: string) => {
     const side = getRef(type).current!;
-    const sidePosArray = side.attributes.position.array as number[];
+    const sidePosArray = getPositionArray(side);
 
     const sideSpecificData = type === "right" ?  getData("x", -40) : type === "left" ? getData("x", 20) : getData("y", 6);
 
@@ -63,12 +74,26 @@ export const Terrain = (props: TerrainProps) => {
   };
 
   const setTerrainElevation = () => {
-    const terrain = beachTerrainRef.current!;
-    const terrainArray = terrain.attributes.position.array as number[];
+    const terrainArray = getPositionArray(beachTerrainRef.current!);
 
     for (let i = 0; i < data.length; i++){
       terrainArray[i * 3 + 2] = data[i].z;
     }
+  };
+
+  const setWaterElevation = () => {
+    const waterArray = getPositionArray(waterRef.current!);
+    const waterData = getData("y", -6);
+
+    for (let i = 0; i < waterData.length; i++){
+      waterArray[i * 3 + 2] = waterData[i].z - .5;
+    }
+
+    const waterRightSideArray = getPositionArray(waterRightSideRef.current!);
+    waterRightSideArray[1] = waterData[0].z;
+
+    const waterLeftSideArray = getPositionArray(waterLeftSideRef.current!);
+    waterLeftSideArray[1] = waterData[3].z;
   };
 
   return (
@@ -77,6 +102,11 @@ export const Terrain = (props: TerrainProps) => {
       {renderPlane([-terrainWidth / 2, 0, 0], [0, -Math.PI / 2, 0], [terrainLength, 1, gridLength], sandTexture, rightSideRef)}
       {renderPlane([terrainWidth / 2, 0, 0], [0, -Math.PI / 2, 0], [terrainLength, 1, gridLength], sandTexture, leftSideRef, THREE.BackSide)}
       {renderPlane([0, 0, -terrainLength / 2], [0, 0, 0], [terrainWidth, 1, gridWidth], sandTexture, backSideRef, THREE.BackSide)}
+
+      {renderPlane([0, .5, 9], [-Math.PI / 2, 0, 0], [terrainWidth, waterLength, gridWidth], waterTexture, waterRef)};
+      {renderPlane([-terrainWidth / 2, 0, 9], [0, -Math.PI / 2, 0], [waterLength, 1], waterTexture, waterRightSideRef)};
+      {renderPlane([terrainWidth / 2, 0, 9], [0, -Math.PI / 2, 0], [waterLength, 1], waterTexture, waterLeftSideRef, THREE.BackSide)}
+      {renderPlane([0, 0, 11], [0, 0, 0], [terrainWidth, 1], waterTexture)}
     </>
   );
 };
