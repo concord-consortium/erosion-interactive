7ea2433f2120d174df20815acbff67f08c32e398
lublagg <lvblagg@gmail.com>
2022-07-07 15:07:29 -0400

    Initial commit.

--- src/ipad/components/3d-components/immersive.tsx
+++ src/ipad/components/3d-components/immersive.tsx
@@ -1,6 +1,5 @@
-import React, { Suspense, useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 import MeshPanaluu from "./mesh-panaluu";
 import Warbear from "./Warbear";
 
@@ -10,36 +9,17 @@ interface IProps {
   selectedBeach?: string
 }
 
-
-const CameraController = () => {
-  const { camera, gl } = useThree();
-
-  useEffect(
-    () => {
-      // camera.rotateZ(Math.PI);
-      const controls = new OrbitControls(camera, gl.domElement);
-      controls.minDistance = 10;
-      controls.maxDistance = 100;
-      controls.enablePan = false;
-      return () => {
-        controls.dispose();
-      };
-    },
-    [camera, gl]
-  );
-  return null;
-};
-
-
 export const Immersive = (props: IProps) => {
   const {selectedBeach} = props;
+  // const {location, direction} = props;
   const PleaseWait = () => <div>Please wait...</div>;
 
   return (
     <div id="immersive" className="canvas-container">
       <Suspense fallback={<PleaseWait/>}>
+        {/*position of camera will be location*/}
         <Canvas camera={ {fov:100, position: [0, 3,-10], near: 0.1}}>
-          <CameraController />
+          {/* direction === shore ? <ShoreViewControls/> : <LandViewControls/> */}
           <ambientLight />
           {/* <pointLight position={[10, 10, 10]} /> */}
           <MeshPanaluu position={[0,0,0]} rotation={[0,0,0]}/>
