4eb60fac7e7a27f5bf1031897e2dfc8fafdfbe37
lublagg <lvblagg@gmail.com>
2022-07-12 13:33:51 -0700

    Added styled UI slider element.

--- src/ipad/components/3d-components/immersive.tsx
+++ src/ipad/components/3d-components/immersive.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { InputHTMLAttributes, Suspense, useEffect, useState } from "react";
 import { Canvas} from "@react-three/fiber";
 import MeshPanaluu from "./mesh-panaluu";
 import { Ruler } from "./ruler";
@@ -7,6 +7,7 @@ import { FirstPersonControls } from "@react-three/drei"
 import { translateCellKeyToGridPosition } from "../../../common/cell-keys-to-ipad";
 
 import "./immersive.scss";
+import { ShoreViewControls } from "./shoreViewControls";
 
 interface IProps {
   selectedBeach?: string;
@@ -18,13 +19,12 @@ interface IProps {
 export const Immersive = (props: IProps) => {
   const {selectedBeach, direction, location} = props;
 
-  const [gridLocation, setGridLocation] = useState<any>();
+  const [gridLocation, setGridLocation] = useState<any>({x: 0, y: 0});
+  const [rotation, setRotation] = useState<any>([0, 0, 0]);
 
   useEffect(() => {
     setGridLocation(translateCellKeyToGridPosition(location));
   }, [location])
-
-  const [rotation, setRotation] = useState<any>([0, 0, 0]);
 
   useEffect(() => {
     setRotation(getRotationFromDirection(direction));
@@ -38,20 +38,8 @@ export const Immersive = (props: IProps) => {
     }
   }
 
-  const renderShoreViewControls = () => {
-    return (
-      <div className="shore-view-controls">
-        {/* code for a slider thing that controls camera goes here*/}
-      </div>
-    )
-  };
-
-  const renderLandViewControls = () => {
-    return (
-      <div className="shore-view-controls">
-        {/* code for a slider thing that controls ruler goes here*/}
-      </div>
-    )
+  const handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    console.log(e.target.value);
   }
 
   const PleaseWait = () => <div>Please wait...</div>;
@@ -59,21 +47,18 @@ export const Immersive = (props: IProps) => {
   return (
     <div id="immersive" className="canvas-container">
       <Suspense fallback={<PleaseWait/>}>
-        {/*position & rotation of camera will be passed down as props*/}
-        <Canvas camera={{fov:100, position: [gridLocation.x, 1, gridLocation.y], rotation: [rotation[0], rotation[1], rotation[2]], near: 0.1}}>
-          <FirstPersonControls
-            activeLook={false}
-            autoForward={false}
-          />
+        {/*position & rotation of camera will be determined by props*/}
+        <Canvas camera={{fov:100, position: [15, 0, -10], near: 0.1}}>
           <axesHelper args={[100]} />
           <Ruler position={[-15, 10, -10]}/>
           <ambientLight />
           <MeshPanaluu position={[0,0,0]} rotation={[0,0,0]}/>
         </Canvas>
-        <div className="controls-overlay">
-          { direction === "shore" ? renderShoreViewControls() : renderLandViewControls()}
-        </div>
       </Suspense>
+      <div className="controls-overlay">
+        <ShoreViewControls handleChange={handleChange}/>
+      </div>
     </div>
+
   );
 };
---
+++ src/ipad/components/3d-components/shoreViewControls.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IControlProps {
+  handleChange: (e: any) => void;
+}
+
+export const ShoreViewControls = (props: IControlProps) => {
+  return (
+      <div className="slider-container">
+        <input onChange={props.handleChange} type="range" min="1" max="100" className="slider" id="myRange"/>
+      </div>
+  )
+}
