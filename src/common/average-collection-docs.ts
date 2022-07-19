c68877747c6fa0b53bb29072e028cfbf59f09a00
lublagg <lvblagg@gmail.com>
2022-07-19 11:55:30 -0400

    Added additional if condition to average collection docs (data field needs to exist).

--- src/common/average-collection-docs.ts
+++ src/common/average-collection-docs.ts
@@ -15,21 +15,22 @@ import { CellKeys } from './constants';
  export const averageDocs = (docs: IErosionDoc[], keys=CellKeys): ErosionData => {
    const results:ErosionData = {};
    const erosionStatus: Record<string, keyCounter> = {};
- 
+
    // Ensure that each cell has a status counter.
    for(const key of keys) {
      erosionStatus[key]||= {count: 0, total: 0};
    }
- 
+
    docs.forEach( (d) => {
      for(const key of keys) {
-       if(d.data[key] !== null && d.data[key] !== undefined) {
+       if(d.data && d.data[key] !== null && d.data[key] !== undefined) {
          const status = erosionStatus[key];
          status.count+= 1;
          status.total = status.total! + d.data[key]!;
        }
      }
    });
+
    for(const key of keys) {
      const status = erosionStatus[key];
      // Don't include cells for which we have no data:
