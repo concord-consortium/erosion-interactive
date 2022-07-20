b761d0b4080b3c5f6b6be83e7c8b2c66f96e91ae
lublagg <lvblagg@gmail.com>
2022-07-19 21:56:12 -0400

    Fixed bug in averageDocs.

--- src/common/average-collection-docs.ts
+++ src/common/average-collection-docs.ts
@@ -15,15 +15,15 @@ import { CellKeys } from './constants';
  export const averageDocs = (docs: IErosionDoc[], keys=CellKeys): ErosionData => {
    const results:ErosionData = {};
    const erosionStatus: Record<string, keyCounter> = {};
- 
+
    // Ensure that each cell has a status counter.
    for(const key of keys) {
      erosionStatus[key]||= {count: 0, total: 0};
    }
- 
+
    docs.forEach( (d) => {
      for(const key of keys) {
-       if(d.data[key] !== null && d.data[key] !== undefined) {
+       if(d.data?.[key] !== null && d.data?.[key] !== undefined) {
          const status = erosionStatus[key];
          status.count+= 1;
          status.total = status.total! + d.data[key]!;
